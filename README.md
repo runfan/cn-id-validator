7b48f68e5f2b2e9319725631518cb24197cd6f42
cwfan <cw.fan@qq.com>
2017-07-27 23:46:09 +0800

    add test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cn-id-validator
 中国公民身份证号码合法性校验
+基于 es6 语法
+
+```javascript
+const Validator = require('cn-id-validator');
+const validator = new Validator('110101196809021004');
+const result = validator.valid()
+```
+```javascript
+false // if faild
+{ address: '北京市东城区', birthday: '1968-09-02', age: 48, gender: 0 } //if success
+```
---
+++ test/id.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const Validator = require('../index');
+describe('CNIDValidator', () => {
+    it('should success return Object', () => {
+        const v = new Validator('11010119670524545X');
+        const info = v.valid()
+        expect(info).to.be.a('object')
+        expect(info.age).to.eq(50)
+        expect(info.birthday).to.eq('1967-05-24');
+        expect(info.gender).to.equal(1)
+    });
+
+    it('should fail and return false', () => {
+        const v = new Validator('21010119670524545X');
+        const info = v.valid()
+        assert(!info)
+    });
+});
